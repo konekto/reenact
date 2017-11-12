418f7374a2e2b41db06ccb25a44e65b95cbb095d
haithem bel haj <haythem.belhaj@gmail.com>
2017-11-12 14:24:22 +0100

    refactor renderer to stream

--- test/renderer.specs.js
+++ test/renderer.specs.js
@@ -1,37 +1,38 @@
-const assert = require('assert');
-const path = require('path');
-const renderer = require('../lib/renderer');
+const assert = require("assert");
+const path = require("path");
+const { renderToString } = require("../lib/renderer");
 
-
-describe('Renderer Specs', function() {
-
-  it('should render component file', function (done) {
-
-    renderer({file: path.resolve(__dirname, 'stubs/component.jsx'), data: {text: 'test'}})
-      .then((html) => {
-
-        assert.equal(html, '<div data-reactroot="">test</div>');
-        done();
-      })
+describe("Renderer Specs", function() {
+  it("should render component file", function(done) {
+    renderToString({
+      file: path.resolve(__dirname, "stubs/component.jsx"),
+      props: { text: "test" }
+    }).then(html => {
+      assert.equal(html, '<div data-reactroot="">test</div>');
+      done();
+    });
   });
 
-  it('should render nested component file', function (done) {
-
-    renderer({file: path.resolve(__dirname, 'stubs/nested.jsx'), data: {text: 'test'}})
-      .then((html) => {
-
-        assert.equal(html, '<div data-reactroot="">test</div>');
-        done();
-      })
+  it("should render nested component file", function(done) {
+    renderToString({
+      file: path.resolve(__dirname, "stubs/nested.jsx"),
+      props: { text: "test" }
+    }).then(html => {
+      assert.equal(html, '<div data-reactroot="">test</div>');
+      done();
+    });
   });
 
-  it('should render component with modules', function (done) {
-
-    renderer({file: path.resolve(__dirname, 'stubs/with-module.jsx'), data: {value: new Date(681436800000)}})
-      .then((html) => {
-
-        assert.equal(html, '<time data-reactroot="">1991-08-06T02:00:00+02:00</time>');
-        done();
-      })
+  it("should render component with modules", function(done) {
+    renderToString({
+      file: path.resolve(__dirname, "stubs/with-module.jsx"),
+      props: { value: new Date(681436800000) }
+    }).then(html => {
+      assert.equal(
+        html,
+        '<time data-reactroot="">1991-08-06T02:00:00+02:00</time>'
+      );
+      done();
+    });
   });
 });
